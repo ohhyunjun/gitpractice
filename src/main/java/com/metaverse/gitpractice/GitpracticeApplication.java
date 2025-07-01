48cb182eb2414e354ed948cf82b7543cf5b6fba5
Hynjun_Oh <dhguswns75@gmail.com>
2025-07-01 15:30:14 +0900

    chore: First Init

---
+++ src/main/java/com/metaverse/gitpractice/GitpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.gitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpracticeApplication.class, args);
+	}
+
+}
